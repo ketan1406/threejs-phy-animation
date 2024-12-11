ea320a83578ed201ca4dcabfb0b2987a75552700
Ketan <sainiketan99@gmail.com>
2024-12-11 15:14:19 +0530

    Config update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 import { defineConfig } from "vite";
 export default defineConfig({
   base: '/threejs-phy-animation/',  // Replace with the correct repo name
+  build: {
+    target: 'esnext',  // Target modern JavaScript, enabling top-level await
+  },
 });
